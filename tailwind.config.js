1022a71baf4d85e2ca4d4225cd908e025b07a0bc
Luke Rohenaz <rohenaz@gmail.com>
2023-11-17 23:36:33 -0500

    aesthetic af

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,54 @@
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./www/**/*.html", "./www/**/*.js", "./www/**/*.ts"],
+  theme: {
+    extend: {},
+  },
   daisyui: {
     themes: [
-      "coffee",
       {
-        streetkid: {
-          primary: "#075985",
-          secondary: "#57534e",
-          accent: "#fb923c",
-          neutral: "#1f2937",
-          "base-100": "#111827",
-          info: "#164e63",
-          success: "#047857",
-          warning: "#713f12",
-          error: "#fca5a5",
+        cyberdark: {
+          'primary' : '#ffee00',
+          'primary-focus' : '#ff5784',
+          'primary-content' : '#000000',
+
+          'secondary' : '#75d1f0',
+          'secondary-focus' : '#5bbedc',
+          'secondary-content' : '#000000',
+
+          'accent' : '#c07eec',
+          'accent-focus' : '#ad55e7',
+          'accent-content' : '#000000',
+
+          'neutral' : '#3d3a00',
+          'neutral-focus' : '#090901',
+          'neutral-content' : '#ffee00',
+
+          "base-100": "#000000",
+          "base-200": "#111111", 
+          "base-300": "#1a1a1a", 
+          "base-content": "#ffee00", 
+
+          info: "#1c92f2", 
+          success: "#009485", 
+          warning: "#c07eec", 
+          error: "#1c92f2", 
+
+          "--rounded-box": "0",
+          "--rounded-btn": "0",
+          "--rounded-badge": "0",
+
+          "--animation-btn": ".25s",
+          "--animation-input": ".2s",
+
+          "--btn-text-case": "uppercase",
+          "--navbar-padding": ".5rem",
+          "--border-btn": "1px",
         },
       },
+
+      "coffee",
+      "cyberpunk",
       "dark",
       "forest",
       "halloween",
